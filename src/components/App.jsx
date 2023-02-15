61458fd69d6bc9c714d170eeb36b82ddae4c0f94
333Nikita333 <nikitosfox333@gmail.com>
2023-02-15 17:59:12 +0200

    finished layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,23 @@
-import { Profile } from 'components/Profile/Profile';
 import user from 'data/user.json';
-
+import data from 'data/data.json';
+import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
 console.log(user);
-
+console.log(data);
 export const App = () => {
   return (
-    <Profile
-      avatar={user.avatar}
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      followers={user.stats.followers}
-      views={user.stats.views}
-      likes={user.stats.likes}
-    ></Profile>
+    <main>
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      ></Profile>
+      <Statistics title="Upload stats" stats={data} />
+      {/* <Statistics stats={data} />; */}
+    </main>
   );
 };
