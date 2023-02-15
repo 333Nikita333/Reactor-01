a6fcb9bd370726ef70cf15a3527b0a9bd9943b7b
333Nikita333 <nikitosfox333@gmail.com>
2023-02-15 20:43:14 +0200

    finished styling FriendList and FriendListItem components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import user from 'data/user.json';
 import data from 'data/data.json';
+import friends from 'data/friends.json';
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from 'components/FriendList/FriendList';
+
 console.log(user);
 console.log(data);
+console.log(friends);
+
 export const App = () => {
   return (
     <main>
@@ -18,6 +23,7 @@ export const App = () => {
       ></Profile>
       <Statistics title="Upload stats" stats={data} />
       {/* <Statistics stats={data} />; */}
+      <FriendList friends={friends} />;
     </main>
   );
 };
